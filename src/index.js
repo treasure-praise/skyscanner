57667f25cce963e0f8d7fd87691c219acea4b04d
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-08-17 13:18:07 +0100

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'bpk-stylesheets/base';
+import 'bpk-stylesheets/base.css';
+import 'bpk-stylesheets/font';
+import 'bpk-stylesheets/font.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
